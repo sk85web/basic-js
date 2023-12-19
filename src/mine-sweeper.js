58c277413f21980f6e326ac762bd5f8153b93782
Sergey <sk85post@gmail.com>
2023-12-19 09:48:10 +0300

    complete mini-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let resultMatrix = []
+  for (let i = 0; i < matrix.length; i++) {
+    let curentArr = []
+    for (let j = 0; j < matrix[i].length; j++) {
+      let cell = 0
+      if (matrix[i][j - 1] && matrix[i][j - 1] === true) cell += 1
+      if (matrix[i][j + 1] && matrix[i][j + 1] === true) cell += 1
+
+      if (matrix[i - 1] && matrix[i - 1][j] === true) cell += 1
+      if (matrix[i - 1] && matrix[i - 1][j - 1] === true) cell += 1
+      if (matrix[i - 1] && matrix[i - 1][j + 1] === true) cell += 1
+
+      if (matrix[i + 1] && matrix[i + 1][j] === true) cell += 1
+      if (matrix[i + 1] && matrix[i + 1][j - 1] === true) cell += 1
+      if (matrix[i + 1] && matrix[i + 1][j + 1] === true) cell += 1
+      curentArr.push(cell)
+    }
+    resultMatrix.push(curentArr)
+  }
+  return resultMatrix
 }
 
 module.exports = {
-  minesweeper
-};
+  minesweeper,
+}
